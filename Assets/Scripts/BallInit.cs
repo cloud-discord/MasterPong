2208a7344fb4a70895d44b9bbfc395cb90ccd971
agente2012 <jonipeter05@hotmail.com>
2017-01-30 23:10:15 +0000

    Added some assets and Scripts for the player!

---
+++ Assets/Scripts/BallInit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallInit : MonoBehaviour {
+
+
+	private Rigidbody2D m_Rigidbody2D;
+	// Use this for initialization
+	private void Awake()
+	{
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+		m_Rigidbody2D.velocity = new Vector2 (-5, -5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PongPlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class PongPlayerControl : MonoBehaviour
+{
+	[SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the y axis.
+
+	private Rigidbody2D m_Rigidbody2D;
+
+	private void Awake()
+	{
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		//do nothing
+	}
+
+	private void FixedUpdate()
+	{
+		// Read the inputs.
+		float move = CrossPlatformInputManager.GetAxis("Vertical");
+		// Pass all parameters to the character control script.
+
+		// Move the character
+		m_Rigidbody2D.velocity = new Vector2(0, move*m_MaxSpeed);
+
+	}
+}
