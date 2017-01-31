ba37883294aad3cff5a298424e5dd7b710ecf693
Alexandre Trincão <alexandre_carros334@hotmail.com>
2017-01-31 18:19:48 +0000

    Criei uma nova scene com o menu que contem o botao play a funcionar

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour {
+
+	public Canvas MainCanvas;
+
+
+
+
+
+	public void LoadOn(){
+		Application.LoadLevel (1);
+	}	
+}
