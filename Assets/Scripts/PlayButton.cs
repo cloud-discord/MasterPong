1111a9178b2b5c79fc98ef1986e7caca53a0877a
Alexandre Trincão <alexandre_carros334@hotmail.com>
2017-02-01 16:14:46 +0000

    Coloquei mais cenas no menu e uma musica no menu, o botao de on/off ainda nao funciona

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -5,7 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour {
 
+	public Canvas MainCanvas;
+	public Canvas OptionsCanvas;
+
+	void Awake(){
+		OptionsCanvas.enabled = false;
+	}
+
+	public void OptionsOn(){
+		OptionsCanvas.enabled = true;
+		MainCanvas.enabled = false;
+	}
+
+	public void ReturnOn(){
+		OptionsCanvas.enabled = false;
+		MainCanvas.enabled = true;
+	}
+		
 	public void LoadOn(){
 		SceneManager.LoadScene (1);
-	}	
+	}
 }
