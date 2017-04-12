b5b75af6e472a1068a3765e8b2edd465ec9738c6
agente2012 <jonipeter05@hotmail.com>
2017-04-13 00:31:15 +0100

    Added Control Tutorial

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
 
-public class PlayButton : MonoBehaviour {
-	private int couter;
+public class MainMenuScript : MonoBehaviour {
 
 	public GameObject MainCanvas;
 	public GameObject OptionsCanvas;
 	public GameObject PlayOptionsCanvas;
+	public GameObject ControlsCanvas;
 
 	void Awake(){
 		try{
 			MainCanvas.SetActive (true);
 			OptionsCanvas.SetActive(false);
 			PlayOptionsCanvas.SetActive(false);
+			ControlsCanvas.SetActive(false);
 		}
 		catch(Exception ex) {
 			Debug.Log (ex.ToString ());
@@ -27,27 +28,45 @@ public class PlayButton : MonoBehaviour {
 			MainCanvas.SetActive (false);
 			OptionsCanvas.SetActive(true);
 			PlayOptionsCanvas.SetActive(false);
+			ControlsCanvas.SetActive(false);
 		}
 		catch(Exception ex) {
 			Debug.Log (ex.ToString ());
 		}
 	}
 
+
 	public void ReturnOn(){
 		try{
 		OptionsCanvas.gameObject.SetActive(false);
 		MainCanvas.gameObject.SetActive(true);
 		PlayOptionsCanvas.SetActive(false);
+		ControlsCanvas.SetActive(false);
 		}
 		catch(Exception ex){
 			Debug.Log (ex.ToString ());
 		}
 	}
+
+
 	public void PlayOptions(){
 		try{
 		OptionsCanvas.SetActive(false);
 		MainCanvas.SetActive(false);
 		PlayOptionsCanvas.SetActive(true);
+		ControlsCanvas.SetActive(false);
+		}
+		catch(Exception ex){
+			Debug.Log (ex.ToString ());
+		}
+	}
+
+	public void openControls(){
+		try{
+			OptionsCanvas.SetActive(false);
+			MainCanvas.SetActive(false);
+			PlayOptionsCanvas.SetActive(false);
+			ControlsCanvas.SetActive(true);
 		}
 		catch(Exception ex){
 			Debug.Log (ex.ToString ());
